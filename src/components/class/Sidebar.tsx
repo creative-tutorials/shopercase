cbdbf6093831d4e40c9412b49d38a1a8ac6cc233
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-12-08 18:43:32 +0100

    ✨ Added link to sidebar

--- src/components/class/Sidebar.tsx
+++ src/components/class/Sidebar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../styles/sidebar_style.css"
+import { Link } from "react-router-dom";
 export function Sidebar({ active, setactive }: any) {
   return (
     <div id="sidebar" className={active ? "active" : "inactive"}>
@@ -14,6 +15,13 @@ export function Sidebar({ active, setactive }: any) {
       <div id="tex">
         <h5>Welcome to ShoperCase Web Application</h5>
         <h3>Upload, Purchase and Sell</h3>
+      </div>
+      <div id="list">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/">Shop</Link></li>
+        <li><Link to="/">About</Link></li>
+        <li><Link to="/">Blog</Link></li>
+        <li><Link to="/">Contact</Link></li>
       </div>
       <div id="btm">
         <h6>Are you following us on social media?</h6>
