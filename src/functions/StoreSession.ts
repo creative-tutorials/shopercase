b39ab975aa55ba49aa866bcc78941ee1afcc1207
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-12-14 16:29:00 +0100

    🌿 Added back routing function

--- src/functions/StoreSession.ts
+++ src/functions/StoreSession.ts
@@ -1,3 +1,6 @@
 export function StoreSession(result: any) {
   localStorage.setItem("auth-session", JSON.stringify(result));
+  setTimeout(() => {
+    window.location.pathname = "/"
+  }, 2000);
 }
