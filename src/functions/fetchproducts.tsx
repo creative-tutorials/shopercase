54a805766da2d36b5a2b7e20c0d3023ad708ffe6
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-12-16 16:53:03 +0100

    🔒️ Fixed Security Issues

--- src/functions/fetchproducts.tsx
+++ src/functions/fetchproducts.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Product } from "../components/client/Product";
 export async function FetchProductsFromAPI() {
+  /* Retrieving the session from localStorage. */
+  const session: any = localStorage.getItem("auth-session");
+  /* Parsing the session from localStorage. */
+  const parsedSession = JSON.parse(session);
+  /* Retrieving the session key from the parsed session. */
+  const sessionKey = parsedSession.session_key;
   try {
-    const response = await fetch("http://localhost:8080/admin/products", {
-      method: "GET",
+    const response = await fetch(`http://localhost:8080/user/${sessionKey}`, {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
         apikey: import.meta.env.VITE_API_KEY,
       },
+      body: JSON.stringify({
+        email: parsedSession.email,
+        password: parsedSession.password
+      })
     });
 
     if (response.ok) {
@@ -20,6 +30,10 @@ export async function FetchProductsFromAPI() {
           <Product result={result} />
         </React.StrictMode>
       );
+    }
+    if (!response.ok) {
+      const result = await response.json();
+      alert(result.message);
     }
   } catch (err) {
     console.error(err);
