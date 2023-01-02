cb40f8088a7ac4419e197de40bc7bfc42b6db780
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2023-01-02 21:11:47 +0100

    🚧 Working on Purchase function

--- src/components/client/Product.tsx
+++ src/components/client/Product.tsx
@@ -1,9 +1,35 @@
-import { HideProductID } from "../../functions/HideProductID";
 export function Product({ result }: any) {
+  const HandlePurchase = () => {
+    result.map(function (item: any, index: any) {
+      FirePurchaseAPI(item);
+    });
+    async function FirePurchaseAPI(item: any) {
+      const session_token: any = localStorage.getItem("auth-session");
+      const parsedData = JSON.parse(session_token);
+      try {
+        const response = await fetch(
+          `http://localhost:8080/products/request/${item.productID}/${parsedData.session_key}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              apikey: import.meta.env.VITE_API_KEY,
+            },
+          }
+        );
+
+        if (response.ok) {
+          const result = await response.json();
+          console.log(result);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
   return (
     <div id="product-flex">
       {result.map(function (item: any, index: any) {
-        HideProductID(item);
         return (
           <div id="items" key={index}>
             <div id="item-image">
@@ -19,7 +45,9 @@ export function Product({ result }: any) {
                   >
                     <i className="fa-regular fa-cart-shopping"></i> Add to cart
                   </button>
-                  <button id="purchase">Purchase</button>
+                  <button id="purchase" onClick={HandlePurchase}>
+                    Purchase
+                  </button>
                 </div>
               </div>
               <img src={item.productImage} alt="" loading="lazy" />
