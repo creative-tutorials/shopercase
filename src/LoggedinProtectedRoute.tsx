f5a480601ae2796498e35a6900b45caf66922b9d
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2023-01-04 18:21:31 +0100

    💂‍♂️ Update  filename

--- src/LoggedinProtectedRoute.tsx
+++ src/LoggedinProtectedRoute.tsx
@@ -3,7 +3,6 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 export default function NewProtectedRoute() {
   const location = useLocation();
   const isAuthenticated = localStorage.getItem("auth-session");
-
   return !isAuthenticated ? (
     <Outlet />
   ) : (
