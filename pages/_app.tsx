a764485556321c8d75c44499f5f66a574096ef42
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2023-04-29 08:28:46 +0100

    ⬆️ Upgraded dependencies 🔊 Added vercel anlytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import "../styles/blurMenu.css"
-import type { AppProps } from 'next/app'
-
+import "../styles/globals.css";
+import "../styles/blurMenu.css";
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} /> 
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
